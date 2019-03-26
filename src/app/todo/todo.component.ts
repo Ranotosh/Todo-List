f8a337ff7e2b30523e14b10365742ebae0b01735
ranotosh <ranotosh.p@transneuron.com>
2019-03-26 15:34:21 +0530

    first_commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit ,TemplateRef, Input, ElementRef} from '@angular/core';
+
+
+interface Task{
+  title: string,
+  is_canceled: boolean
+ }
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  tasks: Array<Task> = [
+    {
+      title: "Go home",
+      is_canceled: false
+    },
+    {
+      title:"Take a nap",
+      is_canceled: false
+    },
+    {
+      title: "Start learning Angular with Sabuj",
+      is_canceled: false
+    }
+  ];
+
+  filtered_tasks: Array<Task> = this.tasks;
+  searchText;
+
+  addTask(input){
+    let value = input.value;
+    input.value = "";
+    this.tasks.push(
+      {
+        title: value,
+        is_canceled: false
+      });
+  }
+ 
+  cancelTask(idx: number){
+    if (this.tasks[idx].is_canceled){
+      this.tasks[idx].is_canceled = false;
+    }else{
+      this.tasks[idx].is_canceled = true;
+    }
+  }
+ 
+  deleteTask(idx: number){
+    let do_delete = confirm("Are you sure to delete the task?");
+    if (do_delete){
+      this.tasks.splice(idx, 1);
+    }
+  }
+ 
+  editTask(idx: number){
+    let title = this.tasks[idx].title;
+    let result = prompt("Edit Task Title", title);
+    if (result !== null && result !== ""){
+      this.tasks[idx].title = result;
+    }
+  }
+
+
+  filterTasks(filter_input){
+    let filter_by: string = filter_input.value;
+    let filtered_tasks: Array<Task> = [];
+    for(let task of this.tasks){
+      if (task.title.includes(filter_by)){
+        filtered_tasks.push(task);
+      }
+    }
+ 
+    this.filtered_tasks = filtered_tasks;
+  }
+
+  clearToDo(index){
+    this.filtered_tasks.splice(index)
+
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
